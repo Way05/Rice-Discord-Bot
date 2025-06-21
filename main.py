423141ea7725f50923065185472b8960f74a06f4
Way05 <130930847+Way05@users.noreply.github.com>
2025-06-20 21:54:22 -0400

    no progress on slash cmds

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -10,8 +9,10 @@ from gemini import getResponse
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
+GUILD_ID = discord.Object(id=1048428980128198677)
+
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 intents.members = True
 
@@ -25,7 +26,7 @@ async def on_ready():
 
     try:
         print("Syncing commands...")
-        synced = await bot.tree.sync()
+        synced = await bot.tree.sync(guild=GUILD_ID)
         print(f"Synced {len(synced)} commands.")
     except Exception as e:
         print(e)
@@ -37,18 +38,17 @@ async def on_message(message):
     if message.author == bot.user:
         return
     
-    if "rice" in message.content.lower():
-        # await message.delete()
-        await message.channel.send(f"{message.author.mention} <:stare:1343032007277412424>")
+    if "rice" in message.content.lower():        await message.channel.send(f"{message.author.mention} <:stare:1343032007277412424>")
 
     await bot.process_commands(message)
 
-@bot.hybrid_command()
-async def test(ctx):
-    await ctx.send("<:stare:1343032007277412424>")
+@bot.tree.command(name="test", guild=GUILD_ID)
+async def test(interaction: discord.Interaction):
+    await interaction.response.send_message(content="<:stare:1343032007277412424>")
 
-@bot.hybrid_command(name="ask", description="ask gemini anything")
+@bot.command(name="ask", description="ask gemini anything")
 async def ask(ctx, *, message):
+    await ctx.defer()
     loading = await ctx.send("Thinking...")
     req = getResponse(message)
     await loading.delete()
