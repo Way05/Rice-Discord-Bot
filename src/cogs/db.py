34bace8166dea30dfd236d94329fe8aa36a424d2
Way05 <130930847+Way05@users.noreply.github.com>
2025-06-29 22:18:09 -0400

    moved /rice to cog.db

--- src/cogs/db.py
+++ src/cogs/db.py
@@ -20,6 +20,17 @@ class DB(commands.Cog):
         await self.bot.db.commit()
         await interaction.response.send_message(f"{interaction.user.mention} has been registered in the database.")
 
+    @app_commands.command(name="rice", description="Check your rice balance")
+    async def rice(self, interaction: discord.Interaction):
+        cursor = await self.bot.db.cursor()
+        await cursor.execute("SELECT rice FROM users WHERE user_id = ?", (interaction.user.id,))
+        res = await cursor.fetchone()
+        if res is None:
+            await interaction.response.send_message("Please register your user using ```/register``` before checking your rice balance.")
+        else:
+            rice_amount = res[0]
+            await interaction.response.send_message(f"{interaction.user.mention}, you have {rice_amount} rice.")
+
 async def setup(bot):
     await bot.add_cog(DB(bot))
 
